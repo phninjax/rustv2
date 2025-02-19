75e567dcd7ac867f08b34950bc8bd6ad427da5bd
kendante <147165759+kendante@users.noreply.github.com>
2025-02-19 14:14:16 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-const btn = document.querySelector('.mbbtn');
-const navlinks = document.querySelector('.navlinks');
-
-
-btn.addEventListener('click', ()=>{
-    navlinks.classList.toggle('mbbtnclicked');
-});
